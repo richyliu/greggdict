7e804b5ed1c87180eddabca3340d16d574d06154
Richard Liu <richy.liu.2002@gmail.com>
2020-05-28 22:04:48 -0700

    start react app

---
+++ react_app/src/components/AppContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AppContainer = () => {
+  return <div className="font-serif text-lg m-3">Hello, world</div>;
+};
+
+export default AppContainer;
---
+++ react_app/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.css',
+    './src/**/*.js',
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+}
