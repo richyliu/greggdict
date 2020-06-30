776ca9cc2aa786a7380178bf7a32928b28cc7ffe
Richard Liu <richy.liu.2002@gmail.com>
2020-06-29 22:05:19 -0700

    release v0.0.4

--- react_app/src/settings.js
+++ react_app/src/settings.js
@@ -1,9 +1,9 @@
 // current version
-const version = 'v0.0.4-prerelease';
+const version = 'v0.0.4';
 
 // root for the reference.json and the pages/ folder for images
 const assetsRoot =
-  'https://cdn.jsdelivr.net/gh/richyliu/greggdict@3ce45f5204decf5886e68fdb6a2be55be22ca257/assets';
+  'https://cdn.jsdelivr.net/gh/richyliu/greggdict@5261c1d7217359c0ac0f23fb021ce006eace7bdb/assets';
 
 // list of available shorthand series
 const series = ['Simplified'];
