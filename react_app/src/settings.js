3c116ffcf7acb2b3c4bb98073d3d3f27f47776f7
Richard Liu <richy.liu.2002@gmail.com>
2020-09-10 12:16:40 -0700

    update dictionary ref

--- react_app/src/settings.js
+++ react_app/src/settings.js
@@ -3,7 +3,7 @@ const version = 'v0.0.5';
 
 // root for the reference.json and the pages/ folder for images
 const assetsRoot =
-  'https://cdn.jsdelivr.net/gh/richyliu/greggdict@5261c1d7217359c0ac0f23fb021ce006eace7bdb/assets';
+  'https://cdn.jsdelivr.net/gh/richyliu/greggdict@89fcdf330d9222674bd46897a5a37f99666b0e4e/assets';
 
 // list of available shorthand series
 const series = ['Simplified'];
