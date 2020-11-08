3b862ce97ae24563bcfd9440723b4c0f0cc04328
Richard Liu <richy.liu.2002@gmail.com>
2020-11-08 11:41:28 -0800

    fix vulnerability, fix spelling of word, and deploy prerelease

--- react_app/src/settings.js
+++ react_app/src/settings.js
@@ -1,9 +1,9 @@
 // current version
-const version = 'v0.0.5';
+const version = 'v0.0.6-prerelease';
 
 // root for the reference.json and the pages/ folder for images
 const assetsRoot =
-  'https://cdn.jsdelivr.net/gh/richyliu/greggdict@89fcdf330d9222674bd46897a5a37f99666b0e4e/assets';
+  'https://cdn.jsdelivr.net/gh/richyliu/greggdict@bf707d46f40ded7a417118ae4595523d699fcc3f/assets';
 
 // list of available shorthand series
 const series = ['Simplified'];
