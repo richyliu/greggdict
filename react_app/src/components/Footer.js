f184d04228a62490e8a8adad3a671a7403b29e01
Richard Liu <richy.liu.2002@gmail.com>
2021-02-19 17:08:12 -0800

    fix accidentally lowercased words, update year

--- react_app/src/components/Footer.js
+++ react_app/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
     <a href="https://github.com/richyliu/greggdict/releases">
       {version}
     </a>{' '}
-    | &copy; Richard Liu 2020
+    | &copy; Richard Liu 2021
   </div>
 );
 
