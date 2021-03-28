9c606042944537b352bb8823279ec57ed5603492
Richard Liu <richy.liu.2002@gmail.com>
2021-03-27 21:12:37 -0700

    release v0.1.0

--- react_app/src/settings.js
+++ react_app/src/settings.js
@@ -1,9 +1,9 @@
 // current version
-const version = 'v0.0.7';
+const version = 'v0.1.0';
 
 // root for the reference.json
 const dictRoot =
-  'https://cdn.jsdelivr.net/gh/richyliu/greggdict@043fd3f8a2e738719080c921d8ac3e518b019a38/assets';
+  'https://cdn.jsdelivr.net/gh/richyliu/greggdict@9dd8240c5b3154addecd9b0c1ed747606f273b03/assets';
 // root for the pages/ folder for images
 const imageRoot =
   'https://cdn.jsdelivr.net/gh/richyliu/greggdict@4b8e49583e141ebc86d0c9bbe19247574b81f2c6/assets';
