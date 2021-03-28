2b617b9ce5ddc6b8261863f25c31208d28fc2301
Richard Liu <richy.liu.2002@gmail.com>
2021-03-28 11:10:44 -0700

    store preferences for show expanded

--- react_app/src/components/ImageContainer.js
+++ react_app/src/components/ImageContainer.js
@@ -4,8 +4,20 @@ import ImageDisplay from './ImageDisplay';
 import ImageControls from './ImageControls';
 import { imageRoot } from '../settings';
 
+const IS_EXPANDED_KEY = 'gregg_dict01_default_is_expanded';
+
 const ImageContainer = ({ word, series }) => {
-  const [isExpanded, setIsExpanded] = useState(true);
+  // load settings for user convenience
+  let stored = localStorage.getItem(IS_EXPANDED_KEY);
+  const [isExpanded, setIsExpanded] = useState(stored === '1');
+
+  function updateIsExpanded(toggler) {
+    setIsExpanded(s => {
+      let newExpanded = toggler(s);
+      localStorage.setItem(IS_EXPANDED_KEY, newExpanded ? '1' : '0');
+      return newExpanded;
+    });
+  }
 
   const image = word && `${imageRoot}/${series}/pages/${word.page}.png`;
 
@@ -15,7 +27,7 @@ const ImageContainer = ({ word, series }) => {
       {word && (
         <ImageControls
           isExpanded={isExpanded}
-          setIsExpanded={setIsExpanded}
+          setIsExpanded={updateIsExpanded}
           fullImageUrl={image}
         />
       )}
