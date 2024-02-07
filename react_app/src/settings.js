ffadc46ffdc2e8f167f49f985311a8135d1bd92f
Richard Liu <richy.liu.2002@gmail.com>
2024-02-07 01:10:11 -0600

    update cdn url for new reference.json

--- react_app/src/settings.js
+++ react_app/src/settings.js
@@ -3,7 +3,7 @@ const version = 'v0.1.0';
 
 // root for the reference.json
 const dictRoot =
-  'https://cdn.jsdelivr.net/gh/richyliu/greggdict@9dd8240c5b3154addecd9b0c1ed747606f273b03/assets';
+  'https://cdn.jsdelivr.net/gh/richyliu/greggdict@fe98009c2c41ea4a36429a002e73d129fdd645c6/assets';
 // root for the pages/ folder for images
 const imageRoot =
   'https://cdn.jsdelivr.net/gh/richyliu/greggdict@4b8e49583e141ebc86d0c9bbe19247574b81f2c6/assets';
